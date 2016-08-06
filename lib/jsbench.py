f4df34481c4195d87d2c92c7e5c51bff5413e57d
Léo Gaspard <leo@gaspard.io>
2016-08-05 17:41:17 -0700

    Add support for jsbench

---
+++ lib/jsbench.py
@@ -0,0 +1,27 @@
+import os
+import subprocess as subp
+import sys
+
+class Bench:
+    moreisbetter = False
+
+    def run(interp):
+        os.chdir(os.path.join(sys.path[0], 'vendor', 'jsbench'))
+        tests = subp.run("echo -n */*/*.js", shell=True, stdout=subp.PIPE,
+                         universal_newlines=True)
+        if tests.returncode != 0:
+            raise "Error when listing the test cases of jsbench!"
+        result = {}
+        for test in tests.stdout.split(' '):
+            res = subp.run([interp, test], stdout=subp.PIPE,
+                           universal_newlines=True)
+            if res.returncode != 0:
+                raise "Error when trying to run jsbench " + test + " with interpreter " + interp + "!"
+            res = res.stdout.split(' ')
+            if len(res) != 2:
+                continue
+            res = res[1].split('m')
+            if len(res) != 2:
+                continue
+            result[test] = float(res[0])
+        return result
